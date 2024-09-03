88428faf6543b0937ff0f1e7777a5ff653b14ba5
Baret <450955+Baret@users.noreply.github.com>
2024-09-03 21:29:54 +0200

    testing Version

--- dependencydb-model/src/main/kotlin/de/gleex/graphdb/playground/model/Version.kt
+++ dependencydb-model/src/main/kotlin/de/gleex/graphdb/playground/model/Version.kt
@@ -7,14 +7,30 @@ data class Version private constructor(
     val patch: Int
 ) {
     companion object {
-        private val VERSION_REGEX = Regex("(<major>\\d+)(.(<minor>\\d+))?(.(<patch>\\d+))?(<suffix>.*)")
+            /**
+         * The Regex to match a valid version.
+         */
+        val VERSION_REGEX = Regex("(?<major>\\d+)(.(?<minor>\\d+))?(.(?<patch>\\d+))?(?<suffix>.*)")
+
+        /**
+         * Creates a new [Version] from the given string. A version needs at least a numerical major version.
+         * Optionally it may contain a minor and patch version and a suffix.
+         *
+         * The format is <major>.<minor>.<patch><suffix>
+         *
+         * @return A new [Version], if [versionString] contains a parsable version
+         * @throws IllegalArgumentException if no version could be extracted from the given string.
+         */
         operator fun invoke(versionString: String): Version {
             val matchResult = VERSION_REGEX.matchEntire(versionString)
+            requireNotNull(matchResult) {
+                "No version detected in given string '$versionString'"
+            }
             return Version(
                 versionString,
-                matchResult?.groups?.get("major")?.value?.toIntOrNull() ?: 0,
-                matchResult?.groups?.get("minor")?.value?.toIntOrNull() ?: 0,
-                matchResult?.groups?.get("patch")?.value?.toIntOrNull() ?: 0
+                matchResult.groups["major"]?.value?.toIntOrNull() ?: 0,
+                matchResult.groups["minor"]?.value?.toIntOrNull() ?: 0,
+                matchResult.groups["patch"]?.value?.toIntOrNull() ?: 0
             )
         }
     }
--- dependencydb-model/src/test/kotlin/de/gleex/graphdb/playground/model/VersionTest.kt
+++ dependencydb-model/src/test/kotlin/de/gleex/graphdb/playground/model/VersionTest.kt
@@ -1,16 +1,50 @@
 package de.gleex.graphdb.playground.model
 
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.Headers4
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.data.table
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.haveMessage
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.Codepoint
+import io.kotest.property.arbitrary.filterNot
+import io.kotest.property.arbitrary.string
+import io.kotest.property.arbitrary.stringPattern
 import io.kotest.property.checkAll
 
-class VersionTest: StringSpec() {
+class VersionTest : StringSpec() {
     init {
         "Valid versions" {
-            TODO()
+            table(
+                Headers4("Version string", "expected major", "expected minor", "expected patch"),
+                row("1.0.0", 1, 0, 0),
+                row("1.0", 1, 0, 0),
+                row("1", 1, 0, 0),
+                row("1.000.00", 1, 0, 0),
+                row("1.2.3", 1, 2, 3),
+                row("123.456.0789", 123, 456, 789)
+            ).forAll { version, expectedMajor, expectedMinor, expectedPatch ->
+                val newVersion = Version(version)
+                assertSoftly(newVersion) {
+                    major shouldBe expectedMajor
+                    minor shouldBe expectedMinor
+                    patch shouldBe expectedPatch
+                }
+            }
         }
 
         "Invalid versions" {
-            TODO()
+            Arb.string().filterNot { it.matches(Version.VERSION_REGEX) }
+                .checkAll { invalidVersionString ->
+                    shouldThrow<IllegalArgumentException> {
+                        Version(invalidVersionString)
+                    } should haveMessage("No version detected in given string '$invalidVersionString'")
+                }
         }
     }
 }
