ded3ba60025bec0fb9acfeedcd25c90701dcd988
Baret <450955+Baret@users.noreply.github.com>
2024-09-06 16:41:09 +0200

    return flow instead of flux

--- neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/controller/ReleaseController.kt
+++ neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/controller/ReleaseController.kt
@@ -5,19 +5,21 @@ import de.gleex.graphdb.playground.model.GroupId
 import de.gleex.graphdb.playground.model.Release
 import de.gleex.graphdb.playground.model.Version
 import de.gleex.graphdb.playground.neo4j.spring.repositories.ReleaseRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.reactive.asFlow
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Flux
 
 @RestController
 @RequestMapping("/release")
 class ReleaseController(private val releaseRepository: ReleaseRepository) {
 
     @GetMapping("/all")
-    suspend fun allReleases(): Flux<Release> {
+    suspend fun allReleases(): Flow<Release> {
         return releaseRepository.findAll()
-//            .asFlow()
+            .asFlow()
             .map {
                 Release(
                     GroupId(it.g),
