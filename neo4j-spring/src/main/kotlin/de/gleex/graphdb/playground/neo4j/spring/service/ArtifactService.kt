fc3329d12c766d70f0b2056985a63b4f918923bd
Baret <450955+Baret@users.noreply.github.com>
2024-10-20 21:20:19 +0200

    debug log all releases

--- neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/service/ArtifactService.kt
+++ neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/service/ArtifactService.kt
@@ -4,10 +4,13 @@ import de.gleex.graphdb.playground.model.Artifact
 import de.gleex.graphdb.playground.model.ArtifactId
 import de.gleex.graphdb.playground.model.GroupId
 import de.gleex.graphdb.playground.neo4j.spring.repositories.ArtifactRepository
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.reactive.asFlow
 import org.springframework.stereotype.Service
+
+private val log = KotlinLogging.logger {  }
 
 @Service
 class ArtifactService(private val artifactRepository: ArtifactRepository) {
@@ -15,6 +18,8 @@ class ArtifactService(private val artifactRepository: ArtifactRepository) {
         return artifactRepository.findAll()
             .asFlow()
             .map {
+                log.info { "Artifact ${it.g}:${it.a} has ${it.releases.size} releases" }
+                it.releases.forEach { r -> log.info { "\t${r.g}:${r.a}:${r.version}" } }
                 Artifact(GroupId(it.g), ArtifactId(it.a))
             }
     }
