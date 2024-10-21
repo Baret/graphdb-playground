779acd1960f2afe30a8289c9aa2facaa5ccac52b
Baret <450955+Baret@users.noreply.github.com>
2024-10-21 12:54:30 +0200

    removed Artifact.releaesesUrl

--- dependencydb-model/src/main/kotlin/de/gleex/graphdb/playground/model/Artifact.kt
+++ dependencydb-model/src/main/kotlin/de/gleex/graphdb/playground/model/Artifact.kt
@@ -3,6 +3,4 @@ package de.gleex.graphdb.playground.model
 data class Artifact(
     val groupId: GroupId,
     val artifactId: ArtifactId
-) {
-    val releasesUrl = "/release/get/${groupId.gId}/${artifactId.aId}"
-}
+)
