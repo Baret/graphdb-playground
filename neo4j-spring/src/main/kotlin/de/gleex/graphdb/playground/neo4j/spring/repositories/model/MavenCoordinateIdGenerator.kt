e8cf4b268bb8e0c54ab3f65b17eb7759ea8fb265
Baret <450955+Baret@users.noreply.github.com>
2024-10-27 16:14:34 +0100

    use simpler maven coordinate as DB ID

--- neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/repositories/model/MavenCoordinateIdGenerator.kt
+++ neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/repositories/model/MavenCoordinateIdGenerator.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 /**
  * Generates maven coordinates as IDs for Artifact and Release entities. Throws an exception for all other labels.
  *
- * The format of a maven coordinate is `groupId:artifactId:jar:1.2.3:compile`
+ * The format of a maven coordinate is `groupId:artifactId:1.2.3`
  * For ArtifactEntity we simply use the prefix of the full coordinate: `groupId:artifactId`
  */
 @Component
@@ -19,7 +19,7 @@ class MavenCoordinateIdGenerator: IdGenerator<String> {
             }
             "Release" -> {
                 val releaseEntity = entity as ReleaseEntity
-                "${releaseEntity.g}:${releaseEntity.a}:jar:${releaseEntity.version}:compile"
+                "${releaseEntity.g}:${releaseEntity.a}:${releaseEntity.version}"
             }
             else -> throw IllegalStateException("Can not generate ID for primaryLabel=$primaryLabel and entity=$entity")
         }
