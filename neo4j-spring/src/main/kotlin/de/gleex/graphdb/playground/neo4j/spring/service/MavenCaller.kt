97ee54482439f8f9080379492845535ec9d820a0
Baret <450955+Baret@users.noreply.github.com>
2024-11-05 22:02:14 +0100

    shortcut for dependency:tree

--- neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/service/MavenCaller.kt
+++ neo4j-spring/src/main/kotlin/de/gleex/graphdb/playground/neo4j/spring/service/MavenCaller.kt
@@ -53,7 +53,6 @@ class MavenCaller(private val config: MavenConfig) {
                     val gIdMatch = matchResult?.groups?.get("groupId")
                     val aIdMatch = matchResult?.groups?.get("artifactId")
                     val versionMatch = matchResult?.groups?.get("version")
-                    log.debug { "MatchResult=$matchResult gIdMatch=$gIdMatch aIdMatch=$aIdMatch versionMatch=$versionMatch" }
                     if (gIdMatch != null && aIdMatch != null && versionMatch != null) {
                         log.debug { "\tFound parent in line: $line" }
                         detectedParent = ReleaseCoordinate(
@@ -73,7 +72,7 @@ class MavenCaller(private val config: MavenConfig) {
         }
     }
 
-    private suspend fun locatePomFile(releaseCoordinate: ReleaseCoordinate): Path? {
+    suspend fun locatePomFile(releaseCoordinate: ReleaseCoordinate): Path? {
         val artifactPomFile: Path = repoBasePath
             .resolve(releaseCoordinate.groupId.gId.replace('.', File.separatorChar))
             .resolve(releaseCoordinate.artifactId.aId)
@@ -96,6 +95,7 @@ class MavenCaller(private val config: MavenConfig) {
             addArgs(DEFAULT_ARGS)
             addArg("-Dartifact=$releaseCoordinate")
             addArg("-Dtransitive=false")
+            setOutputHandler { log.debug { "[mvnGet] $it" } }
         }
         if (invocationSuccessful.not()) {
             return null
@@ -111,65 +111,69 @@ class MavenCaller(private val config: MavenConfig) {
     }
 
     private suspend fun resolveDependencies(releaseCoordinate: ReleaseCoordinate, pomPath: Path): List<Dependency> {
-        log.info { "Invoking maven to get dependencies for pom file $pomPath" }
-
         val depTreeFileName = "${releaseCoordinate.toString().replace(":", "_")}_depTree.txt"
-        val invocationSuccessful = executeMaven("get dependencies for file $pomPath") {
-            goals = listOf("$PLUGIN:tree")
-            pomFile = pomPath.toFile()
-            addArgs(DEFAULT_ARGS)
-            addArg("-DoutputEncoding=UTF-8")
-            addArg("-Dtokens=whitespace")
-            addArg("-DoutputFile=$depTreeFileName")
-            setOutputHandler { log.debug { "[depTree] $it" } }
-        }
-        if (invocationSuccessful.not()) {
-            return emptyList()
+        val depTreeFile = pomPath.resolveSibling(depTreeFileName)
+        if (depTreeFile.toFile().exists()) {
+            log.debug { "Found existing dependency tree file ${depTreeFile.absolutePathString()} for release $releaseCoordinate" }
         } else {
-            val depTreeFile = pomPath.resolveSibling(depTreeFileName)
-            if (!depTreeFile.exists()) {
-                log.error { "Could not create dependency tree file at ${depTreeFile.absolutePathString()}" }
+            log.info { "Invoking maven to get dependencies for pom file $pomPath" }
+            val invocationSuccessful = executeMaven("get dependencies for file $pomPath") {
+                goals = listOf("$PLUGIN:tree")
+                pomFile = pomPath.toFile()
+                isRecursive = false
+                addArgs(DEFAULT_ARGS)
+                addArg("-DoutputEncoding=UTF-8")
+                addArg("-Dtokens=whitespace")
+                addArg("-DoutputFile=$depTreeFileName")
+                setOutputHandler { log.debug { "[depTree] $it" } }
+            }
+            if (invocationSuccessful.not()) {
                 return emptyList()
             }
-            val treeFileLines = depTreeFile.toFile().readLines(Charsets.UTF_8)
-            // TODO: Check that the first line is "de.gleex.kng:kotlin-name-generator-examples:jar:0.1.0"
+        }
+
+        if (!depTreeFile.exists()) {
+            log.error { "Could not create dependency tree file at ${depTreeFile.absolutePathString()}" }
+            return emptyList()
+        }
+        val treeFileLines = depTreeFile.toFile().readLines(Charsets.UTF_8)
+        // TODO: Check that the first line is "de.gleex.kng:kotlin-name-generator-examples:jar:0.1.0"
 //            check(treeFileLines.first() == releaseCoordinate.toString()) {
 //                "Dependency tree file ${depTreeFile.absolutePathString()} does not belong to requested release coordinate $releaseCoordinate"
 //            }
-            val dependencies: MutableList<Dependency> = mutableListOf()
-            val lastParentOnDepth: MutableMap<Int, ReleaseCoordinate> = mutableMapOf(0 to releaseCoordinate)
-            log.debug { "Starting tree traversal with parent map: $lastParentOnDepth" }
-            treeFileLines.drop(1)
-                .forEach { line ->
-                    var depth = -1
-                    var reducingLine = line
-                    while (reducingLine.startsWith("   ")) {
-                        depth++
-                        reducingLine = reducingLine.substring(3)
-                    }
-                    // each artifact is listed in the form of <gId>:<aId>:jar:<version>:compile
-                    val coordinateComponents = reducingLine.split(':')
-                    val dependencyCoordinate = ReleaseCoordinate(
-                        GroupId(coordinateComponents[0]),
-                        ArtifactId(coordinateComponents[1]), Version(coordinateComponents[3])
-                    )
-                    val treeParent = lastParentOnDepth[depth]
-                    checkNotNull(treeParent) {
-                        "No parent dependency found in tree at depth ${depth - 1} for $dependencyCoordinate"
-                    }
-                    lastParentOnDepth[depth + 1] = dependencyCoordinate
-                    log.debug { "Put parent on depth $depth to ${lastParentOnDepth[depth]}" }
-                    log.debug { "Found parent $treeParent for depth $depth - $dependencyCoordinate" }
-                    dependencies += Dependency(
-                        dependencyCoordinate,
-                        depth,
-                        treeParent
-                    )
-                        .also { log.debug { "Adding dependency $it" } }
+        val dependencies: MutableList<Dependency> = mutableListOf()
+        val lastParentOnDepth: MutableMap<Int, ReleaseCoordinate> = mutableMapOf(0 to releaseCoordinate)
+        log.debug { "Starting tree traversal with parent map: $lastParentOnDepth" }
+        treeFileLines.drop(1)
+            .forEach { line ->
+                var depth = -1
+                var reducingLine = line
+                while (reducingLine.startsWith("   ")) {
+                    depth++
+                    reducingLine = reducingLine.substring(3)
                 }
-            return dependencies
-                .also { log.info { "Found ${it.size} dependencies for release $releaseCoordinate" } }
-        }
+                // each artifact is listed in the form of <gId>:<aId>:jar:<version>:compile
+                val coordinateComponents = reducingLine.split(':')
+                val dependencyCoordinate = ReleaseCoordinate(
+                    GroupId(coordinateComponents[0]),
+                    ArtifactId(coordinateComponents[1]), Version(coordinateComponents[3])
+                )
+                val treeParent = lastParentOnDepth[depth]
+                checkNotNull(treeParent) {
+                    "No parent dependency found in tree at depth ${depth - 1} for $dependencyCoordinate"
+                }
+                lastParentOnDepth[depth + 1] = dependencyCoordinate
+                log.debug { "Put parent on depth $depth to ${lastParentOnDepth[depth]}" }
+                log.debug { "Found parent $treeParent for depth $depth - $dependencyCoordinate" }
+                dependencies += Dependency(
+                    dependencyCoordinate,
+                    depth,
+                    treeParent
+                )
+                    .also { log.debug { "Adding dependency $it" } }
+            }
+        return dependencies
+            .also { log.info { "Found ${it.size} dependencies for release $releaseCoordinate" } }
     }
 
     private suspend fun executeMaven(
